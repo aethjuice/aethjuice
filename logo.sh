a84e31b94a0b941681cc6aa3fbe5c87a2b6d20f3
A Σ Th. <dkuring29@gmail.com>
2024-09-13 13:36:20 +0700

    Add files via upload

---
+++ logo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+GREEN="\e[32m"
+AE='\033[0m'
+
+echo -e "${GREEN}"
+echo "               __.....__     "
+echo "           .-''         '.    "
+echo "          /     .-''"'-.  `.  "
+echo "    __   /     /________\   \"
+echo " .:--.'. |                  |"
+echo "/ |   \ |\    .-------------'"
+echo "`" __ | | \    '-.____...---."
+echo " .'.''| |  `.             .' "
+echo "/ /   | |_   `''-...... -'   "
+echo "\ \._,\ '/                   "
+echo "  `--'  `"                   "
+echo ""
+echo "Join our discord https://discord.gg/aetherealco"
+echo -e "${AE}"
