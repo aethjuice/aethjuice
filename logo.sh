098966b29ef32d97032bdcf9d43d2b72110bf82a
A Σ Th. <dkuring29@gmail.com>
2024-09-13 13:42:55 +0700

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 
-# Define colors using a more readable format
-GREEN="\033[32m"
-RESET="\033[0m"
+GREEN="\e[32m"
+AE='\033[0m'
 
-# Define the logo as a string array
-logo=("
-    __.....__
-    .-''      '.
-    /  .-''"'-.  `.
-    __/  /________\  \"
-   .:--.'. !          !
-   / |  \ |\  .-------------'
-   `" __ | | \  '-.____...---."
-    .'.''| |  `.           .'
-    / /  | |_  `''-...... -'
-    \ \._,\ '/            
-     `--'  `"
-")
-
-# Print the logo with color and reset
-for line in "${logo[@]}"; do
-    echo -e "${GREEN}$line${RESET}"
-done
-
+echo -e "${GREEN}"
+echo -e "               __.....__     "
+echo -e "           .-''         '.    "
+echo -e "          /     .-''"'-.  `.  "
+echo -e "    __   /     /________\   \"
+echo -e " .:--.'. !                  !"
+echo -e "/ |   \ |\    .-------------'"
+echo -e "`" __ | | \    '-.____...---."'
+echo -e " .'.''| |  `.             .' "
+echo -e "/ /   | |_   `''-...... -'   "
+echo -e "\ \._,\ '/                   "
+echo -e "  `--'  `"                   "
 echo ""
 echo "Join our discord https://discord.gg/aetherealco"
+echo -e "${AE}"
